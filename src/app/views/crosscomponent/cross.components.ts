1fba7d6c7ac939076aec77029cad23fb09c4e16d
AekJ4s <Jasdakornam@gmail.com>
2024-04-29 15:42:48 +0700

    Finish first angular

--- src/app/views/crosscomponent/cross.components.ts
+++ src/app/views/crosscomponent/cross.components.ts
@@ -1,11 +1,13 @@
-import { Component, Input, Output  } from '@angular/core';
+import { Component, Input, Output ,   } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { EventEmitter } from 'stream';
 
 @Component({
   selector: 'app-cross',
   templateUrl: './cross.components.html',
   styleUrls: ['./cross.components.css'],
-  standalone: true
+  standalone: true,
+  imports: [FormsModule]
 })
 export class appcrossComponent {
   @Input() data={ IdData: 0, nameData: "Simple Data" };
